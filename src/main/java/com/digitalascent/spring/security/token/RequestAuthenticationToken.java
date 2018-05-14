5e1dcdccd796e5263383503f19c9d8d375c7a464
Chris Lee <chris@digitalascent.com>
2018-05-13 18:51:34 -0700

    Updated

--- src/main/java/com/digitalascent/spring/security/token/RequestAuthenticationToken.java
+++ src/main/java/com/digitalascent/spring/security/token/RequestAuthenticationToken.java
@@ -1,6 +1,8 @@
 package com.digitalascent.spring.security.token;
 
+import com.digitalascent.common.base.SecureValues;
 import com.digitalascent.spring.security.AuthenticationDetails;
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 
@@ -11,22 +13,29 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public final class RequestAuthenticationToken extends AbstractAuthenticationToken {
     private static final long serialVersionUID = 123456L;
 
-    private final String apiToken;
+    private final String token;
 
-    public RequestAuthenticationToken(String apiToken, AuthenticationDetails details) {
+    public RequestAuthenticationToken(String token, AuthenticationDetails details) {
         super(ImmutableList.of());
-        this.apiToken = checkNotNull(apiToken, "apiToken is required");
+        this.token = checkNotNull(token, "token is required");
         setDetails(checkNotNull(details, "details is required"));
     }
 
     @Override
     public Object getCredentials() {
-        return apiToken;
+        return token;
     }
 
     @Override
     @Nullable
     public Object getPrincipal() {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("token", SecureValues.maskTrailing(token,5))
+                .toString();
     }
 }
