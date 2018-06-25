58c6a738a94d3cf2dd0bc5553232bc075800a691
Chris Lee <chris@digitalascent.com>
2018-06-25 11:25:44 -0700

    Fix dupe event publishing

--- src/test/java/com/digitalascent/spring/security/token/RequestTokenAuthenticationFilterTest.java
+++ src/test/java/com/digitalascent/spring/security/token/RequestTokenAuthenticationFilterTest.java
@@ -32,7 +32,7 @@ public class RequestTokenAuthenticationFilterTest {
     @Before
     public void setup() {
         AuthenticationManager authenticationManager = Mockito.mock(AuthenticationManager.class);
-        filter = new RequestTokenAuthenticationFilter(TokenIdentifierExtractors.headerValue("x-api-key"), authenticationManager, new DefaultAuthenticationEventPublisher() );
+        filter = new RequestTokenAuthenticationFilter(TokenIdentifierExtractors.headerValue("x-api-key"), authenticationManager );
         request = new MockHttpServletRequest();
         response = new MockHttpServletResponse();
         filterChain = new MockFilterChain();
